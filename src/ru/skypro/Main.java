dbe276867d73f7bbece6bf585686200088742a06
Veronica <kqadb@mail.ru>
2022-04-17 23:21:06 +0300

    hw7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,47 @@
 package ru.skypro;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = (lastName + " " + firstName + " " + middleName);
+        System.out.println(fullName);
+
+        String fullName1 = fullName.toUpperCase(Locale.ROOT);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета " + fullName1);
+
+        StringBuilder fullNameNew = new StringBuilder("Ivanov Ivan Ivanovich");
+        fullNameNew.delete(0, 21);
+        fullNameNew.append("Иванов Семён Семёнович");
+        fullNameNew = fullNameNew.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника " + fullNameNew);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
